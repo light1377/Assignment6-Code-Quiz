21af353e880415fb49ecbaf96894cb3fe2089c20
Moral <moral@meshkot.com>
2023-02-10 02:02:10 +0000

    added basic structure and sample questions

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,28 @@
-{/* <button id="start-button">Start</button> */}
+var startButton = document.querySelector(".start-button");
+
+{/* <button id="start-button">Start</button> */ }
+
+//mke "start" button start questions and timer
+// Button.onClick = function() {
+
+// }
+
+function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+}
+
+function startTimer() {
+
+}
+
+//function that shows and checks answers
+
+
+
+
+
