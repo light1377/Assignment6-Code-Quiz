09609037256f938f96450bf6e3abceef174bb283
Moral <moral@meshkot.com>
2023-02-24 23:42:36 +0000

    added questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,4 +1,7 @@
-var questions = [
+const questions = {
+    Q1 : "What does HTML stand for?",
+    Q2 : "What does CSS stand for?",
+    Q3 : "What does js stand for?",
+    Q4 : "Is flexbox 1-dimentional or 2-dimentional?",
+}
 
-
-];
